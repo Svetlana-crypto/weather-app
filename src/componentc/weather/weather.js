8a2ac9bf743d2a8c6ca1ee54a4d31699f0d7525c
Svetlana-crypto <vip.ssveta04@gmail.com>
2021-09-23 23:27:17 +0600

    first commit

--- src/componentc/weather/weather.js
+++ src/componentc/weather/weather.js
@@ -14,19 +14,15 @@ const Weather = ({weatherData}) => {
 
   return (
     <div>
-      <p>Город:{weatherData.name}</p>
+      <p>Город : {weatherData.name}</p>
       <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt="Icon Weather"/>
-      <p>Температура:{(weatherData.main.temp - 273.15).toFixed(2)}°C</p>
-      <p>Давление:{weatherData.main.pressure} Па</p>
-      <p>Влажность:{weatherData.main.humidity} %</p>
-      <p>Время:{formattedTime(weatherData.dt)} </p>
-      <p>Восход:{formattedTime(weatherData.sys.sunrise)} </p>
-      <p>Закат:{formattedTime(weatherData.sys.sunset)} </p>
-      <p>Скорость ветра:{weatherData.wind.speed} m.s</p>
-
-
-
-
+      <p>Температура : {(weatherData.main.temp - 273.15).toFixed(2)} °C</p>
+      <p>Давление : {weatherData.main.pressure} Па</p>
+      <p>Влажность : {weatherData.main.humidity} %</p>
+      <p>Время : {formattedTime(weatherData.dt)} </p>
+      <p>Восход : {formattedTime(weatherData.sys.sunrise)} </p>
+      <p>Закат : {formattedTime(weatherData.sys.sunset)} </p>
+      <p>Скорость ветра : {weatherData.wind.speed} м.с</p>
     </div>
   );
 };
